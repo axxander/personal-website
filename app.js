a7018ee37934752ede6ed2b1046af1df9b6a1b9a
Alex Gregory <axander.gregory@gmail.com>
2021-07-29 00:29:15 +0100

    fixing deliberate error: testing automated testing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({ statuss: 'building' });
+  res.json({ status: 'building' });
 });
 
 // 404 for all other routes
