2b5708dd6bf84f490aa253ae7f730674299e4737
Alex Gregory <axander.gregory@gmail.com>
2021-08-30 19:47:30 +0100

    updating homepage

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({ status: 'hello meg' });
+  res.json({ status: 'hello Kitt' });
 });
 
 // 404 for all other routes
